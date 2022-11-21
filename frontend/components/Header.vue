4902567009575d97173073f0b443f425081905c1
Terry Crapts <tcrapts@gmail.com>
2022-11-21 10:50:21 +0100

    add styles

---
+++ frontend/components/Header.vue
@@ -0,0 +1,171 @@
+<template>
+  <header id="header">
+    <div class="logo-wrapper ma-0 pa-0" id="mainwrapper">
+      <figure>
+        <img
+          alt="Rijksoverheid Logo"
+          src="../assets/logo-ro-zonder-caption.svg"
+          class="logo-full"
+        />
+        <img
+          alt="Rijksoverheid Logo"
+          src="../assets/logo-ro-zonder-caption-mobile.svg"
+          class="logo-mobile"
+        />
+        <figcaption class="logo-caption">Rijksoverheid</figcaption>
+      </figure>
+    </div>
+  </header>
+
+  <div class="bar-wrapper" id="bar">
+    <div class="bar-wrapper-content">
+      <div>
+        {{ title }}
+      </div>
+      <nav>
+        <ul>
+          <li v-for="p in pages" :key="p">
+            <router-link
+              :to="{ path: '/' + p.toLowerCase() }"
+              aria-current="page"
+              >{{ p }}</router-link
+            >
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  pages?: string[];
+}>();
+</script>
+
+<style scoped lang="scss">
+.logo-wrapper figure {
+  display: flex;
+  align-items: flex-start;
+}
+
+.logo {
+  height: 76px;
+}
+.logo-wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 70px;
+}
+
+.logo-wrapper img {
+  margin-left: 90px;
+  height: 70px;
+}
+
+.bar-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $primary;
+  height: 76px;
+}
+
+.bar-wrapper nav,
+.bar-wrapper div {
+  padding: 0 8px;
+}
+
+.bar-wrapper-content {
+  max-width: 1200px;
+  display: flex;
+  font-size: 26px;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.bar-wrapper-content div a,
+.bar-wrapper-content nav {
+  color: white;
+}
+
+.logo-caption {
+  font-family: RO Serif, Calibri, sans-serif;
+  font-size: 1rem;
+  line-height: 1.1;
+  width: 100%;
+  max-width: 300px;
+  padding: 20px 10px 10px;
+  color: #000;
+}
+
+nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 80%;
+}
+
+nav ul {
+  list-style: none;
+  display: table;
+  vertical-align: middle;
+}
+
+nav ul > li {
+  display: inline-block;
+  vertical-align: middle;
+  padding: 0 0 0 1em;
+}
+
+.barwapper ul > li a {
+  color: white;
+  text-decoration: none;
+}
+
+.bar-wrapper-content nav a {
+  color: white;
+  text-decoration: none;
+}
+
+.bar-wrapper-content nav a:hover {
+  text-decoration: underline;
+}
+
+nav ul > li a:hover {
+  text-decoration: underline;
+}
+
+@media (min-width: 768px) {
+  .logo-caption {
+    padding: 50px 12px 25px;
+  }
+
+  .logo-wrapper {
+    height: 125px;
+  }
+
+  .logo-wrapper img {
+    height: 125px;
+    margin-left: 112px;
+  }
+
+  img.logo-full {
+    display: block;
+  }
+
+  img.logo-mobile {
+    display: none;
+  }
+}
+
+.logo-mobile {
+  display: block;
+}
+
+.logo-full {
+  display: none;
+}
+</style>
