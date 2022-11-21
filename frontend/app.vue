b0ae781fced2c760b930b1a79017280d2dc5fe89
Terry Crapts <terry.crapts@ictu.nl>
2022-11-21 10:50:21 +0100

    add styles

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    Test
-    <NuxtWelcome />
-  </div>
+  <RijksoverheidHeaderVue
+    title="Applicatienaam"
+    :pages="['about']"
+  />
+  <NuxtPage />
 </template>
+
+<script setup lang="ts">
+import RijksoverheidHeaderVue from "@/components/Header.vue";
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+  css: ["@/assets/styles/main.scss"],
+});
