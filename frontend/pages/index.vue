007084758f30cffeb735b0fa3c9b75e2bb542925
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-11-22 15:58:08 +0100

    Merge remote-tracking branch 'origin' into lars

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,30 @@
-<script setup>
-import Page from '~~/components/PageWrapper.vue'
-
-definePageMeta({
-  title: 'Home page',
-})
-</script>
-
 <template>
-  <Page>Home page index</Page>
+  <Page :title="`Homepage`">
+    <v-row>
+      <v-col :cols="10">
+        <v-text-field
+          v-model="searchQuery"
+          label="Zoek op organisatie, titel of omschrijving"
+          variant="outlined"
+          @keyup.enter="doSearch"
+        ></v-text-field>
+      </v-col>
+      <v-col :cols="2"
+        ><v-btn color="secondary" size="x-large" @click="doSearch"
+          >Zoek</v-btn
+        ></v-col
+      >
+    </v-row>
+  </Page>
 </template>
+
+<script setup>
+import Page from '@/components/PageWrapper.vue'
+
+const searchQuery = ref('')
+
+const doSearch = () => {
+  const router = useRouter()
+  router.push(`/algoritme?q=${searchQuery.value}`)
+}
+</script>
