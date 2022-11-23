a6ee7f3c79c261234d45798c5feb4ce801dc2978
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-11-23 12:29:25 +0100

    temp

--- frontend/services/algoritme.ts
+++ frontend/services/algoritme.ts
@@ -1,11 +1,11 @@
 import { apiFetch } from '@/utils/fetchApi'
 
 const getAll = () =>
-  apiFetch('/project').then((response) => {
+  apiFetch('/algoritme').then((response) => {
     return response.data
   })
 
 const getOne = (id: string) =>
-  apiFetch(`/project/${id}`).then((response) => response.data)
+  apiFetch(`/algoritme/${id}/`).then((response) => response.data)
 
 export default { getAll, getOne }
