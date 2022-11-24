1b5735970d6cb8029b15304e6443492c6bb6be18
Terry Crapts <tcrapts@gmail.com>
2022-11-24 13:23:27 +0100

    use typing

---
+++ frontend/types/algoritme.ts
@@ -0,0 +1,11 @@
+export interface Algoritme {
+  id: string
+  name: string
+  organization: string
+  department: string
+  description_short: string
+  type: string
+  category: string
+  website: string
+  status: string
+}
