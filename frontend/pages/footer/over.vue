8d5eafe8d1484310bb564ea190aa7e1f2bd3431d
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-12-01 13:03:29 +0100

    textual changes

--- frontend/pages/footer/over.vue
+++ frontend/pages/footer/over.vue
@@ -1,126 +1,71 @@
 <template>
   <Page>
-    <h1>{{ t('paths./over') }}</h1>
     <div v-if="!englishSelected">
-      <!-- <ol> -->
-      <!-- <li> -->
-      <h2>1. Wat is het Algoritmeregister?</h2>
+      <h1>Over het Algoritmeregister van de overheid</h1>
       <p class="mb-4 ml-8">
-        Op het algoritmeregister vindt u informatie over algoritmes in gebruik
-        door de overheid.
+        De overheid werkt steeds meer digitaal en gebruikt daarvoor steeds meer
+        algoritmes. Omdat digitalisering niet automatisch goed gaat, wil de
+        overheid werken aan transparante en verantwoorde inzet van die
+        algoritmes. Zodat de informatie over de algoritmes van de overheid voor
+        iedereen beschikbaar is: burgers, hun belangenbehartigers, media en
+        (overheids)professionals.
       </p>
-      <!-- </li> -->
-      <!-- <li> -->
-      <h2>2. Doel Website</h2>
+      <h2>Open ontwikkelen</h2>
       <p class="mb-4 ml-8">
-        Op het algoritmeregister vindt u informatie over algoritmes in gebruik
-        door de overheid. De entiteiten die verantwoordelijk zijn voor deze
-        algoritmes leveren de gegevens aan.
+        Op algoritme.overheid.nl komen steeds meer algoritmes van de overheid te
+        staan. Deze site werkt al, maar is nog volop in ontwikkeling. Het
+        ministerie van Binnenlandse Zaken en Koninkrijksrelaties ontwikkelt het
+        register op een open manier: iedereen mag meekijken.
       </p>
-      <!-- <p class="mb-4 ml-8">
-        Het register laat de aantallen, kosten en voortgang van de grote
-        ICT-projecten zien. Op de startpagina staat een overzicht van alle
-        projecten, een grafische presentatie met rijksbrede informatie en
-        trendgegevens en een overzicht van de meest actuele wijzigingen. Zoals
-        nieuwe en aangepaste projecten, of grote en kleine aanpassingen in de
-        projectgegevens.
-      </p> -->
+      <h2>Meedoen</h2>
       <p class="mb-4 ml-8">
-        Op de algoritmepagina's staan specifieke informatie over de projecten.
+        (Overheids)professionals kunnen zich nu al aansluiten. Wat kun je doen? 
+        <ul>
+          <li>
+            Stel de algoritmes van je eigen overheidsorganisaties via het Algoritmeregister van de overheid beschikbaar voor het publiek.
+          </li>
+          <li>
+            Denk mee over de verdere ontwikkeling van het Algoritmeregister. 
+          </li>
+        </ul>
+Doe je mee? Neem dan direct contact op: algoritmeregister@minbzk.nl.
+
       </p>
+      <h2>Open source en toegankelijkheid</h2>
       <p class="mb-4 ml-8">
-        Het algoritmeregister is een website van de rijksoverheid. Het register
-        valt onder de verantwoordelijkheid van de minister van Binnenlandse
-        Zaken en Koninkrijksrelaties.
+        Algoritme.overheid.nl wordt ontwikkeld met opensource software (volg ons op Github). Deze site wordt ontwikkeld zodat deze voor zoveel mogelijk mensen bruikbaar is. Dat betekent dat we werken met de eisen voor digitale toegankelijkheid. 
       </p>
-      <!-- </li> -->
-      <!-- <li> -->
-      <!-- <h2>3. Grote ICT projecten</h2>
-      <p class="mb-4 ml-8">
-        Grote ICT-projecten zijn de projecten met een ICT-component van meer dan
-        €5 miljoen over de gehele looptijd van het project. Met de meerjarige
-        ICT-kosten worden de producten, diensten en resultaten van het project
-        gerealiseerd. De ICT-kosten zijn opgenomen in de rijksbegroting, of in
-        de begroting van de publieksrechtelijke zelfstandig bestuursorganen
-        (zbo’s), en worden hierin verantwoord.
-      </p>
-      <p class="mb-4 ml-8">
-        Ieder jaar staat in de Jaarrapportage Bedrijfsvoering Rijk een overzicht
-        van alle in dat jaar actieve projecten, met de belangrijkste data en
-        cijfers. Een geactualiseerde versie van dit jaarlijkse overzicht is
-        hieronder beschikbaar. Als een project in het geselecteerde jaar nog
-        geen wijzigingen heeft doorgevoerd, dan wordt de laatst bekende situatie
-        getoond.
-      </p>
-      <p class="mb-4 ml-8">
-        <b
-          >Download hieronder het CSV-bestand. Dit bestand is circa 1 MB en kunt
-          u openen met een spreadsheetprogramma, zoals Microsoft Excel of
-          LibreOffice Calc.</b
-        >
-      </p> -->
-      <!-- </li> -->
-      <!-- <li> -->
-      <h2>3. Actualiteit</h2>
-      <p class="mb-4 ml-8">
-        De informatie over de algoritmes op het register werken we regelmatig
-        bij; per project minimaal één keer per jaar. Ook als de Tweede Kamer
-        over het project wordt geïnformeerd, worden de gegevens geactualiseerd.
-      </p>
-      <!-- </li> -->
-      <!-- <li> -->
-      <h2>4. Wijzigingen binnen het algoritme</h2>
-      <p class="mb-4 ml-8">
-        Er kunnen belangrijke wijzigingen plaatsvinden die gevolgen hebben voor
-        de inhoud.
-        <!-- De reden van deze wijzigingen wordt altijd aan de
-        Tweede Kamer gemeld. -->
-      </p>
-      <p class="mb-4 ml-8">
-        Een wijziging kan om verschillende redenen noodzakelijk zijn. Denk aan
-        een nieuwe – politieke – eis of aanpassingen vanwege een uitgevoerde
-        kwaliteitstoets.
-        <!-- Meer
-        informatie over de verschillenden redenen en aanpassingen vindt u op de
-        startpagina. -->
-      </p>
-      <!-- </li> -->
-      <!-- <li> -->
-      <!-- <h2>5. Open Data</h2>
-      <p class="mb-4 ml-8">
-        Alle gegevens uit het Algoritmeregister zijn ook als open data
-        beschikbaar. Dat geldt voor de lopende en de afgeronde projecten. De
-        gegevens vindt u in json-formaat en in xml-formaat.
-      </p>
-      <p class="mb-4 ml-8">
-        Deze datasets van het Algoritmeregister zijn ook vermeld in het Open
-        Dataportaal van de rijksoverheid.
-      </p>
-      <p class="mb-4 ml-8">
-        De gegevens van een specifiek project kunt u downloaden. Op de
-        projectpagina staat rechtsboven de optie ‘Exporteer project’. Hiermee
-        downloadt u het CSV-bestand (met komma-gescheiden waarden). Dit bestand
-        is circa 10 kB en kunt u openen met een spreadsheetprogramma, zoals
-        Microsoft Excel of LibreOffice Calc.
-      </p> -->
-      <!-- </li> -->
-      <!-- </ol> -->
+      
     </div>
 
     <div v-if="englishSelected">
-      <p class="mb-4">
-        This website lists information on algorithms currently used by the Dutch
-        government.
+      <h1>About the Algorithm register of the government</h1>
+      <p class="mb-4 ml-8">
+        The government increasingly works digitally and uses more and more algorithms when doing so. Since digitalization not automatically works well, the government wants to provide transparency and responsible use of these algorithms. Information about the algorithms used by the government is available for all: citizens, their advocates, the media and (government) professionals.
       </p>
-      <p class="mb-4">
-        This site currently is still in its infancy and more information and
-        functionality will be added in time.
+      <h2>Open development</h2>
+      <p class="mb-4 ml-8">
+        More and more algorithms used by the Dutch government will be published at algoritme.overheid.nl. This site is live, but is under constant development. The Ministry of the Interior and Kingdom relations is using an open development method to develop the register, meaning everyone can view our progress and join in.
       </p>
-      <p class="mb-4">
-        This website is an initiative of the ministry of Interior Affairs and
-        Kingdom Relations in close co operation with all parts of central,
-        regional and local government of the Netherlands.
+      <h2>Joining in</h2>
+      <p class="mb-4 ml-8">
+        (Government) professionals can join already. How can you contribute?
+        <ul>
+          <li>
+            Make the algorithms of your own government organization available to the public via the algorithm register 
+          </li>
+          <li>
+            Make suggestions for further development of the algorithm register. 
+          </li>
+        </ul>
+        Interested? Contact us via algoritmeregister@minbzk.nl.
+
       </p>
+      <h2>Open source and accessibility</h2>
+      <p class="mb-4 ml-8">
+        Algoritme.overheid.nl wordt ontwikkeld met opensource software (volg ons op Github). Deze site wordt ontwikkeld zodat deze voor zoveel mogelijk mensen bruikbaar is. Dat betekent dat we werken met de eisen voor digitale toegankelijkheid. 
+      </p>
+      
     </div>
   </Page>
 </template>
