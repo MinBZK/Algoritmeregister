fb06d46e87b6c6ca396c47cf4ab46be216d47b7f
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-12-02 14:58:13 +0100

    new homepage searchfunction styling

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,21 +5,12 @@
         {{ homepageTitle }}
       </h1>
     </div>
-    <v-card elevation="0" color="tertiary">
-      <div class="card-margins">
-        <h4 class="homepage-subtitle">
-          {{ homepageSubtitle }}
-        </h4>
-        <v-row>
-          <SearchFunction
-            v-bind:value="searchQuery"
-            @input="(v) => (searchQuery = v)"
-            @doSearch="doSearch"
-          >
-          </SearchFunction>
-        </v-row>
-      </div>
-    </v-card>
+    <SearchFunction
+      v-bind:value="searchQuery"
+      @input="(v) => (searchQuery = v)"
+      @doSearch="doSearch"
+    >
+    </SearchFunction>
   </Page>
 </template>
 
@@ -27,22 +18,13 @@
 import Page from '@/components/PageWrapper.vue'
 import { useI18n } from 'vue-i18n'
 import qs from 'qs'
-import { useDisplay } from 'vuetify'
 import SearchFunction from '@/components/SearchFunction.vue'
 
-const { mdAndDown } = useDisplay()
 const { t } = useI18n()
-const searchHint = computed(() => t('searchHint'))
 const homepageTitle = computed(() => t('homepageTitle'))
-const homepageSubtitle = computed(() => t('homepageSubtitle'))
-const search = computed(() => t('search'))
 
 const searchQuery = ref('')
 
-const showChange = computed(() => {
-  console.log('trigger')
-  return 1
-})
 const doSearch = () => {
   const router = useRouter()
   const stringifiedQuery = qs.stringify({
@@ -53,24 +35,9 @@ const doSearch = () => {
 </script>
 
 <style scoped lang="scss">
-@import '/assets/styles/main.scss';
-.btn-dark {
-  background-color: $primary;
-  color: white;
-}
-.card-margins {
-  padding-left: 80px;
-  padding-top: 40px;
-  padding-right: 80px;
-  padding-bottom: 40px;
-}
 .homepage-title {
   color: #154273;
   text-align: center;
   margin-bottom: 50px;
-}
-.homepage-subtitle {
-  color: #154273;
-  margin-bottom: 10px;
 }
 </style>
