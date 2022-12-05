40e9444089d1439a61e0aee84f015501c3c0d8f7
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-12-05 15:16:09 +0100

    restyled detailpagina

--- frontend/pages/algoritme/[id].vue
+++ frontend/pages/algoritme/[id].vue
@@ -1,4 +1,5 @@
-<template>
+
+<!-- <template>
   <Page>
     <h2>
       {{ algoritme.name }}
@@ -8,7 +9,6 @@
         {{ algoritme.description_short }}
       </v-col>
     </v-row>
-    <!-- {{ structuredProperties }} -->
     <v-row class="mt-3">
       <v-col v-for="sT in summaryTiles"
         ><h4>
@@ -46,6 +46,68 @@
       </v-expansion-panels>
     </v-row>
     <div>''</div>
+  </Page>
+</template> -->
+
+<template>
+  <Page>
+    <ClientOnly>
+      <div class="skiplinks container">
+        <a href="#content">Direct naar content</a>
+      </div>
+      <div class="container row">
+        <NuxtLink class="link cta__backwards" :to="`/algoritme/`">
+          {{ t('goBack') }}
+        </NuxtLink>
+      </div>
+      <div class="container row container--centered">
+        <h1>{{ algoritme.name }}</h1>
+        <div class="well well--pageblock">
+          <!-- <h3>{{ shortDescription }}</h3> -->
+          <p> {{ algoritme.description_short || shortDescriptionMissing }}</p>
+          <!-- <p>
+            <a href="#" class="link link--forward">Alle datasets van deze eigenaar</a>
+          </p> -->
+          <dl class="dl columns--data">
+            <div v-for="sT in summaryTiles">
+              <dt>
+                {{ $t(`algorithmProperties.algemeneInformatie.${sT}.label`) }}
+              </dt>
+              <dd>
+                <span class="icon icon--housing">{{
+                    algoritme[sT as keyof typeof algoritme] || t('Ontbreekt')
+                }}</span>
+              </dd>
+            </div>
+          </dl>
+        </div>
+        <div class="tabs" data-decorator="init-tabs">
+          <ul class="tabs__list" role="tablist">
+            <li role="presentation" v-for="(p, index) in structuredProperties">
+              <span @click="activeAttributeKey = p.attributeGroupKey" :class="[
+                p.attributeGroupKey == activeAttributeKey ? 'is-selected' : '',
+              ]" role="tab" :aria-controls="`panel-${index + 1}`">{{ p.attributeGroupKeyLabel }}</span>
+            </li>
+          </ul>
+          <div class="tabs__panels">
+            <div v-for="(property, index) in activeAttributeProperties" role="tabpanel"
+              :aria-labelledby="`tab-${index + 1}`">
+              <p class="mt-2">
+              <h4> {{ property.attributeKeyLabel }} </h4>
+              </p>
+              <p class="mb-1">
+                <i> {{ property.attributeKeyDescription }} </i>
+              </p>
+              <p class="mb-1">
+                {{ property.attributeValue || t('Ontbreekt') }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </ClientOnly>
+
   </Page>
 </template>
 
@@ -87,9 +149,16 @@ const enrichedAlgoritme = computed(() => {
   return { [groupKey]: group, ...algoritme.value }
 })
 
-// const title = computed(() => algoritme?.value.name)
-
 const { t } = useI18n()
+const shortDescription = computed(() => t('short-description'))
+const shortDescriptionMissing = computed(() => t('short-description-missing'))
+
+let activeAttributeKey = ref('')
+const activeAttributeProperties = computed(() => {
+  return structuredProperties.value.filter((groupedProperty) => {
+    return groupedProperty.attributeGroupKey == activeAttributeKey.value
+  })[0]?.properties
+})
 
 const structuredProperties = computed(() => {
   const algoritme = enrichedAlgoritme
@@ -113,8 +182,8 @@ const structuredProperties = computed(() => {
             typeof value != 'boolean'
               ? value
               : value == true
-              ? t(`yes`)
-              : t(`no`)
+                ? t(`yes`)
+                : t(`no`)
           return {
             attributeKey: key,
             attributeValue: parsedValue,
@@ -127,12 +196,6 @@ const structuredProperties = computed(() => {
           }
         })
         .filter((attribute) => {
-          // console.log(
-          //   requiredFields.properties[
-          //     attribute.attributeKey as keyof typeof requiredFields.properties
-          //   ]?.required,
-          //   attribute.attributeKey
-          // )
           return (
             requiredFields.properties[
               attribute.attributeKey as keyof typeof requiredFields.properties
@@ -146,10 +209,39 @@ const structuredProperties = computed(() => {
 definePageMeta({
   title: 'Algoritme details',
 })
+
+onMounted(() => activeAttributeKey = ref(structuredProperties.value[0].attributeGroupKey))
 </script>
 
 <style scoped lang="scss">
-.text-field-sheet {
-  background-color: $quaternary;
+// These are similar styles to '.tabs__list a' options from _koop_main.scss
+.tabs__list span {
+  display: inline-block;
+  text-align: center;
+  padding: 0.5em 0.75em;
+  background: #fff;
+  text-decoration: none;
+  position: relative;
+  border-top: 2px solid transparent;
+  cursor: pointer;
+}
+
+.tabs__list span:hover {
+  color: #154273;
+  background-color: #f3f3f3;
+}
+
+.tabs__list span.is-selected,
+.tabs__list span[aria-selected='true'] {
+  border-left: 1px solid #e6e6e6;
+  border-right: 1px solid #e6e6e6;
+  border-top: 2px solid #154273;
+  border-bottom-color: #fff;
+  bottom: -1px;
+}
+
+.tabs__list span.is-selected:hover,
+.tabs__list span[aria-selected='true']:hover {
+  background-color: #fff;
 }
 </style>
