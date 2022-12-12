129b08d2cf2190a081718bbbce2a18d49fceeff7
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-12-12 11:00:55 +0100

    add 'home' to breadcrumb

--- frontend/components/views/AppBreadcrumb.vue
+++ frontend/components/views/AppBreadcrumb.vue
@@ -30,23 +30,22 @@ const currentRoute = useRoute()
 
 const navigationItemsTranslated = computed(() =>
   navigationItems.map((item) => {
-    return { label: t(item.label), routeName: item.routeName }
+    return { label: t(item.localeName), routeName: item.routeName }
   })
 )
 
-const navigationItemsWithCustomLink = [
-  {
-    label: 'footer',
-    routeName: ' ',
-  },
-]
+const ignoredNavigationItems = ['footer']
 
 const breadcrumbs = computed(() => {
   const path = currentRoute.path
-  const breadCrumbs = path != '/' ? path.split('/').slice(1) : []
+  // the added '/ ' is interpreted in the mapping as the link to Home
+  var crumbStrings: string[] =
+    path != '/' ? ('/ ' + path).split('/').slice(1) : []
 
-  return breadCrumbs.map((crumb) => {
-    // console.log(nameList.value.find((alg) => alg.slug == crumb)?.name)
+  crumbStrings = crumbStrings.filter(
+    (crumb) => !ignoredNavigationItems.includes(crumb)
+  )
+  return crumbStrings.map((crumb: any) => {
     return {
       label:
         // translate path parts with a name matching a navigation item
@@ -54,9 +53,7 @@ const breadcrumbs = computed(() => {
           ?.label || // translate path parts with a name matching an algorithm slug
         algoritme.value?.name ||
         crumb,
-      routeName:
-        navigationItemsWithCustomLink.find((item) => item.label == crumb)
-          ?.routeName || crumb,
+      routeName: crumb,
     }
   })
 })
