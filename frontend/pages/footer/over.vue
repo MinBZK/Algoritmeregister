32c125e74b00675560ea7189b9893d981a29b4dd
lars.musters@ictu.nl <lars.musters@ictu.nl>
2022-12-15 11:37:04 +0100

    Nieuwe footer teksten en andere kleine aanpassingen

--- frontend/pages/footer/over.vue
+++ frontend/pages/footer/over.vue
@@ -1,94 +1,122 @@
 <template>
   <div>
     <div v-if="!englishSelected">
-      <h1>Over het Algoritmeregister van de overheid</h1>
+      <h1>Over het Algoritmeregister</h1>
       <p class="mb-4">
-        De overheid werkt steeds meer digitaal en gebruikt daarvoor steeds meer
-        algoritmes. Omdat digitalisering niet automatisch goed gaat, wil de
-        overheid werken aan transparante en verantwoorde inzet van die
-        algoritmes. Zodat de informatie over de algoritmes van de overheid voor
+        De overheid werkt steeds meer digitaal en gebruikt daarvoor steeds vaker
+        algoritmes. Omdat digitalisering niet vanzelfsprekend goed gaat, werkt
+        de overheid aan transparante en verantwoorde inzet van die algoritmes.
+        Zodat de informatie over de algoritmes die de overheid gebruikt voor
         iedereen beschikbaar is: burgers, hun belangenbehartigers, de media en
-        (overheids)professionals.
+        toezichthouders.
       </p>
-      <h2>Open ontwikkelen</h2>
+      <h2>Open ontwikkeling</h2>
       <p class="mb-4">
-        Op algoritme.overheid.nl komen steeds meer algoritmes van de overheid te
-        staan. Deze site werkt al, maar is nog volop in ontwikkeling. Het
-        ministerie van Binnenlandse Zaken en Koninkrijksrelaties ontwikkelt het
-        register op een open manier: iedereen mag meekijken.
-      </p>
-      <h2>Meedoen</h2>
-      <p class="mb-4">
-        Ben je (overheids)professional? Dan kan je nu al aansluiten.
-      </p>
-      <p class="mb-4">
-        Wat kan je doen? Stel de algoritmes van je eigen organisaties
-        beschikbaar voor het publiek via het Algoritmeregister. Of denk mee over
-        de verdere ontwikkeling van het Algoritmeregister.
-      </p>
-      <p class="mb-4">
-        Doe je mee? Neem dan contact op met:
-        <a href="mailto:algoritmeregister@minbzk.nl">
-          algoritmeregister@minbzk.nl</a
-        >.
-      </p>
-      <h2>Open source software</h2>
-      <p class="mb-4">
-        algoritme.overheid.nl wordt ontwikkeld met open source software (volg
-        ons op
+        Het Algoritmeregister is in ontwikkeling. De informatie verandert nog
+        vaak. Nieuwe algoritmes worden toegevoegd en algoritmes worden
+        geactualiseerd. Ook bouwt het ministerie van Binnenlandse Zaken en
+        Koninkrijksrelaties steeds door aan het Algoritmeregister. Dit doet het
+        team op een open manier. Omdat het open source software is, kan iedereen
+        meekijken op de
         <a
-          target="_blank"
           class="is-external"
           href="https://github.com/MinBZK/Algoritmeregister"
-          >Github</a
-        >). Deze site wordt ontwikkeld zodat deze voor zoveel mogelijk mensen
-        bruikbaar is.
+        >
+          Github van ministerie van Binnenlandse Zaken en
+          Koninkrijksrelaties.</a
+        >
+      </p>
+      <p class="mb-4">
+        Die open ontwikkeling betekent dat de inhoud door de tijd heen zal
+        wijzigen, bijvoorbeeld:
       </p>
     </div>
 
     <div v-if="englishSelected">
-      <h1>About the Algorithm register of the government</h1>
+      <h1>About the Algorithm Register</h1>
       <p class="mb-4">
         The government increasingly works digitally and uses more and more
-        algorithms when doing so. Since digitalisation not automatically works
-        well, the government wants to provide transparency and responsible use
-        of these algorithms. Information about the algorithms used by the
-        government is available for all: citizens, their supporters, the media
-        and (government) professionals.
+        algorithms when doing so. Since digitalization does not always work
+        well, the government wants to work towards transparent and responsible
+        use of these algorithms. Therefore, the government is making information
+        about the algorithms used by the government available to everyone:
+        citizens, their representatives, the media and supervisors.
       </p>
       <h2>Open development</h2>
       <p class="mb-4">
-        More and more algorithms used by the Dutch government will be published
-        at algoritme.overheid.nl. This site is live and under continuous
-        development. The Ministry of the Interior and Kingdom relations is using
-        an open development method to develop the register, meaning everyone can
-        view our progress and join in.
+        The Algorithm Register is under development. The information often
+        changes. New algorithms are added, and algorithms are updated. The
+        Ministry of the Interior and Kingdom Relations will continue to build
+        the Algorithm Register. The team working on the register is using an
+        open development style, through open source software. Anyone can view
+        the team’s progress on the
+        <a
+          class="is-external"
+          href="https://github.com/MinBZK/Algoritmeregister"
+        >
+          GitHub of the Ministry of the Interior and Kingdom Relations</a
+        >. This open development style means that the content will change over
+        time, for example:
       </p>
-      <h2>Joining in</h2>
+      <ul type="1" class="ml-4">
+        <li>Algorithms are added, or algorithms are removed</li>
+        <li>
+          The 'standard' (fields of information) is changed and/or expanded
+        </li>
+        <li>
+          The published description of algorithms is changed and/or expanded.
+        </li>
+      </ul>
+
+      <h2>Open participation</h2>
       <p class="mb-4">
-        Are you a (government) professional? Then you can join in. Make the
-        algorithms of your own government organisation available to the public
-        via the Algorithm register. Or make suggestions for further development
-        of the Algorithm register.
+        A number of government organisations have already published algorithms
+        in the Algorithm Register. The aim is for all government organisations
+        with relevant algorithms to publish their algorithms. Eventually,
+        registration will also become a legal requirement.
       </p>
       <p class="mb-4">
-        Please contact us at:
+        Government organisations can provide algorithms for publication in the
+        Algorithm Register.
+      </p>
+      <p class="mb-4">
+        There are several areas that you can participate by giving:
+      </p>
+      <ul type="1" class="ml-4">
+        <li>Feedback on the 'standard'</li>
+        <li>Feedback on the published description</li>
+        <li>Feedback on the website’s user experience (UX)</li>
+        <li>Feedback on the code of this website.</li>
+      </ul>
+
+      <p class="mb-4">
+        Would you like to participate? Do you have feedback on one of these
+        topics? Please contact:
         <a href="mailto:algoritmeregister@minbzk.nl">
           algoritmeregister@minbzk.nl</a
         >.
       </p>
       <h2>Open source software</h2>
       <p class="mb-4">
-        algoritme.overheid.nl is developed with open source software (follow us
+        The Algorithm Register is developed with open source software (follow us
         on
         <a
           target="_blank"
           class="is-external"
           href="https://github.com/MinBZK/Algoritmeregister"
           >Github</a
-        >
-        ). This website is being developed to serve as many people as possible.
+        >). This website is being developed to serve as many people as possible.
       </p>
+      <!-- <h2>Algoritmebeleid</h2>
+      <p class="mb-4">
+        ** nog te vertalen **
+        <a
+          class="is-external"
+          href="https://open.overheid.nl/repository/ronl-0de79e5c4c0c9b203c0a1c263efca7eca410958b/1/pdf/bijlage-1-werkagenda-waardengedreven-digitaliseren.pdf"
+        >
+          werkagenda Waardegedreven Digitaliseren</a
+        >.
+      </p> -->
     </div>
   </div>
 </template>
