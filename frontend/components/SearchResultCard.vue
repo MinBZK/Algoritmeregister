f78153ff7a6d91c1646f54b9803a71393701a55f
Lars Musters <lars.musters@ictu.nl>
2022-12-15 12:16:31 +0000

    Migrated the summary at the top of [slug] to searchResultCard

--- frontend/components/SearchResultCard.vue
+++ frontend/components/SearchResultCard.vue
@@ -1,19 +1,19 @@
 <template>
-  <li class="item">
-    <div class="item-header">
+  <li :class="`item ${mode}`">
+    <div v-if="mode === 'compact'" class="item-header">
       <NuxtLink :to="`/algoritme/${algoritme.slug}`" class="result--title">
         <h2>{{ props.algoritme.name }}</h2>
       </NuxtLink>
     </div>
     <p>
-      {{ truncatedDescription }}&nbsp;<NuxtLink
-        v-if="isTruncated"
+      {{ description }}&nbsp;
+      <NuxtLink
+        v-if="isTruncated && mode === 'compact'"
         :to="`/algoritme/${algoritme.slug} `"
         >{{ readMore }}
       </NuxtLink>
     </p>
 
-    <!-- <p> -->
     <dl class="dl columns--data">
       <div v-for="sT in summaryTiles" :key="sT">
         <dt>{{ t(`algorithmProperties.${sT}.label`) }}</dt>
@@ -22,7 +22,6 @@
         </dd>
       </div>
     </dl>
-    <!-- </p> -->
   </li>
 </template>
 
@@ -30,13 +29,18 @@
 import { useI18n } from 'vue-i18n'
 import { summaryTiles } from '@/config/config'
 import type { Algoritme } from '@/types/algoritme'
+interface Props {
+  algoritme: Algoritme
+  mode?: 'compact' | 'default'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  mode: 'default',
+})
 
 const { t } = useI18n()
 const readMore = computed(() => t('searchResultCard.readMore'))
-
-const props = defineProps<{
-  algoritme: Algoritme
-}>()
+const shortDescriptionMissing = computed(() => t('short-description-missing'))
 
 const length = 300
 const truncatedDescription = computed(() => {
@@ -45,6 +49,10 @@ const truncatedDescription = computed(() => {
     ? props.algoritme.description_short
     : truncatedString + '...'
 })
+const description =
+  props.mode === 'compact'
+    ? truncatedDescription
+    : props.algoritme.description_short || shortDescriptionMissing
 
 const isTruncated = computed(() => {
   return (
@@ -66,5 +74,30 @@ h2 {
 
 .no-bottom-margin {
   margin-bottom: 0;
+}
+li.default {
+  background: $tertiary !important;
+  list-style: none;
+  padding: 1em;
+  margin-bottom: 0.5em;
+  border: 0;
+}
+@media (min-width: 51em) {
+  li.default {
+    padding: 1.5em 10% !important;
+  }
+}
+
+li.compact {
+  background: #fff;
+  list-style: none;
+  padding: 1em;
+  margin-bottom: 0.5em;
+  border: 0;
+}
+@media (min-width: 51em) {
+  li.compact {
+    padding: 1.75em 1.25em 1.5em 2em !important;
+  }
 }
 </style>
