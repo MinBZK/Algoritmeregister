0e98cb722674bbc7e93d9f7eb0525cae50153a14
terry.crapts@ictu.nl <terry.crapts@ictu.nl>
2022-12-16 14:58:34 +0100

    adjust labels, load new data

--- backend/app/etl/load.py
+++ backend/app/etl/load.py
@@ -2,6 +2,6 @@ from app.etl.resources.loader import AlgoritmeLoader
 
 
 def load():
-    excel_file = "app/etl/data/2022_12_15_Masterlijst_Algoritmeregister.xlsx"
+    excel_file = "app/etl/data/2022_12_16_Masterlijst_Algoritmeregister.xlsx"
     loader = AlgoritmeLoader(excel_file=excel_file)
     return loader.load_algoritmes()
